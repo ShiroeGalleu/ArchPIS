0339d4b55d66a1e10a8aa60cc68567ebfe088959
ShiroeGalleu <alexanderallen8719@gmail.com>
2024-07-02 00:52:21 -0500

    Last Build of V0.1

--- ArchPIS.sh
+++ ArchPIS.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-
 #v0.1
 
 #add multilib and update the packages
 while true; do
-    read -p "Do you wish enable multilib? Required and will update your pc [y/n] " yn
+    read -p "Do you wish enable multilib? This will also run -Syu Required [y/n] " yn
     case $yn in
         [Yy]* ) sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf && sudo pacman -Syu; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#add git
+while true; do
+    read -p "Do you wish to install git? Required [y/n]" yn
+    case $yn in
+        [Yy]* ) sudo pacman -S git; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -25,22 +34,19 @@ makepkg -si; break;;
     esac
 done
 
-#add pamac appstore
+#add pamac-aur appstore
 while true; do
-    read -p "Do you wish to install pamac? Recommended [y/n]" yn
+    read -p "Do you wish to install pamac-aur? Recommended [y/n]" yn
     case $yn in
-        [Yy]* ) git clone https://aur.archlinux.org/archlinux-appstream-data-pamac.git
-cd archlinux-appstream-data-pamac
-makepkg -si; break;;
+        [Yy]* ) git clone https://aur.archlinux.org/pamac-aur.git
+        cd pamac-aur
+        makepkg -si; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-
-
-
-#install wine and it's
+#install wine and it's dependencies
 while true; do
     read -p "Do you wish to install wine [y/n]" yn
     case $yn in
@@ -55,12 +61,85 @@ lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader l
     esac
 done
 
+#add a msg in the terminal
+echo
+echo
+echo "time for some media"
+echo
+echo
 
+#add Spotify
 while true; do
-    read -p "Do you wish to install this program? [y/n]" yn
+    read -p "Do you wish to install spotify? [y/n]" yn
     case $yn in
-        [Yy]* ) make install; break;;
-        [Nn]* ) exit;;
+        [Yy]* ) git clone https://aur.archlinux.org/spotify.git
+        cd spotify
+        makepkg -si; break;;
+        [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+#add vlc
+while true; do
+    read -p "Do you wish to install VLC? [y/n]" yn
+    case $yn in
+        [Yy]* ) sudo pacman -S vlc; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+#add a msg in the terminal
+echo
+echo
+echo "time for some gaming"
+echo
+echo
+
+#add Steam
+while true; do
+    read -p "Do you wish to install steam? [y/n]" yn
+    case $yn in
+        [Yy]* ) sudo pacman -S steam-native-runtime; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#add heroic
+while true; do
+    read -p "Do you wish to install Heroic? [y/n]" yn
+    case $yn in
+        [Yy]* ) git clone https://aur.archlinux.org/heroic-games-launcher-bin.git
+        cd heroic-games-launcher-bin/
+         makepkg -si; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#add a msg in the terminal
+echo
+echo
+echo "Utility Time"
+echo
+echo
+
+#add docker
+while true; do
+    read -p "Do you wish to install docker? [y/n]" yn
+    case $yn in
+        [Yy]* ) sudo pacman -S docker; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+echo 
+echo 
+echo 
+echo "Thank You for choosing Shiroe Gallue for your post install setup"
+echo 
