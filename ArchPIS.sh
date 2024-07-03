d2ddc9a7b58ff545acd4b47a168752a5f7afdac7
Shiroe Galleu <alexanderallen8719@gmail.com>
2024-07-03 04:11:51 -0500

    Updated 7/3/24

--- ArchPIS.sh
+++ ArchPIS.sh
@@ -6,16 +6,16 @@ clear
 echo "welcome to Shrioe Galleu ArchPIS (Arch Post Install Script)"
 sleep 2
 clear
-echo "multilib and git is required to use this script and will be install if you continue"
+echo "multilib and git is needed use this script"
 sleep 2
 clear
 
 #enable multilib and install git
 while true; do
-    read -p "Do you wish enable multilib, and install git? This will also run -Syu (Required) [y/n] " yn
+    read -p "Do you wish enable multilib, and install git? This will also run -Syu (HIGHLY RECOMMENDED) [y/n] " yn
     case $yn in
         [Yy]* ) sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf && sudo pacman -Syu git; break;;
-        [Nn]* ) exit;;
+        [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -29,7 +29,8 @@ while true; do
         [Yy]* ) pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si; break;;
+makepkg -si
+cd ..; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -37,13 +38,31 @@ done
 
 clear
 
+#graphic drivers
+while true; do
+    read -p "Do you wish to setup your graphics drivers
+    1. AMD (just install reccomened packages)
+    2. Nvida (open)
+    3. Nvida (closed)
+    0. Go to the next step
+
+    " yn
+    case $yn in
+        [1]* ) sudo pacman -S --noconfirm mesa lib32-mesa mesa-vdpau lib32-mesa-vdpau lib32-vulkan-radeon vulkan-radeon glu lib32-glu vulkan-icd-loader lib32-vulkan-icd-loader; break;;
+        [2]* ) Echo "not yet added";;
+        [3]* ) sudo pacman -S --noconfirm nvidia nvidia-settings nvidia-utils lib32-nvidia-utils lib32-opencl-nvidia opencl-nvidia libvdpau libxnvctrl vulkan-icd-loader lib32-vulkan-icd-loader; break;;
+        [0]* ) break;;
+        * ) echo "invalid option";;
+    esac
+done
+
+clear
+
 #add pamac-aur appstore
 while true; do
-    read -p "Do you wish to install pamac-aur? Recommended [y/n]" yn
+    read -p "Do you wish to install pamac-aur? (require yay) Recommended [y/n]" yn
     case $yn in
-        [Yy]* ) git clone https://aur.archlinux.org/pamac-aur.git
-        cd pamac-aur
-        makepkg -si; break;;
+        [Yy]* ) yay -S pamac-aur; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -55,7 +74,7 @@ clear
 while true; do
     read -p "Do you wish to install wine? (Reccomened) [y/n]" yn
     case $yn in
-        [Yy]* ) sudo pacman -S --needed wine giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+        [Yy]* ) sudo pacman -S --needed --noconfirm wine giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
 mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
 lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
 sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
@@ -71,8 +90,8 @@ clear
 #web browsers
 while true; do
     read -p "Web broswer
-    1. Brave
-    2. Google Chrome
+    1. Brave (require yay)
+    2. Google Chrome (require yay)
     3. Chromium
     4. Firefox
     5. Vivaldi
@@ -80,19 +99,13 @@ while true; do
 
     " yn
     case $yn in
-        [1]* ) git clone https://aur.archlinux.org/brave-bin.git
-        cd brave-bin
-        makepkg -si
+        [1]* ) yay -S brave-bin ;;
+        [2]* ) yay -S google-chrome ;;
+        [3]* ) sudo pacman -S --noconfirm chromium 
         echo ;;
-        [2]* ) git clone https://aur.archlinux.org/google-chrome.git
-        cd google-chrome
-        makepkg -si 
+        [4]* ) sudo pacman -S --noconfirm firefox 
         echo ;;
-        [3]* ) sudo pacman -S chromium 
-        echo ;;
-        [4]* ) sudo pacman -S firefox 
-        echo ;;
-        [5]* ) sudo pacman -S vivaldi 
+        [5]* ) sudo pacman -S --noconfirm vivaldi 
         echo ;;
         [0]* ) break;;
         * ) echo "invalid option";;
@@ -105,21 +118,17 @@ clear
 while true; do
     read -p "Game Launchers
     1. steam (Do I really have to cover this one?)
-    2. heroic (Covers Epic, GOG, and Amazon Games)
-    3. legendary (Epic Launcher)
+    2. heroic (Covers Epic, GOG, and Amazon Games) (require yay)
+    3. legendary (Epic Launcher) (require yay)
     4. lutirs (A libary for all your games)
     0. Go to the next step
 
     " yn
     case $yn in
-        [1]* ) sudo pacman -S steam-native-runtime;;
-        [2]* ) git clone https://aur.archlinux.org/heroic-games-launcher-bin.git
-        cd heroic-games-launcher-bin/
-         makepkg -si;;
-        [3]* ) git clone https://aur.archlinux.org/legendary.git
-        cd legendary
-        makepkg -si;;
-        [4]* ) sudo pacman -S lutris;;
+        [1]* ) sudo pacman -S --noconfirm steam-native-runtime;;
+        [2]* ) yay -S --noconfirm heroic-games-launcher-bin ;;
+        [3]* ) yay -S --noconfirm legendary;;
+        [4]* ) sudo pacman -S --noconfirm lutris;;
         [0]* ) break;;
         * ) echo "invalid option";;
     esac
@@ -127,19 +136,26 @@ done
 
 clear
 
+#game utils
+while true; do
+    read -p "do you wish to install reccomened game util? [y/n]" yn
+    case $yn in
+    [Yy]* ) sudo pacman -S mangohud gamemode winetricks gamescope ;;
+    [Nn]* ) break;;
+
+clear
+
 #social
 while true; do
     read -p "Spcial
     1. discord
-    2. vesktop (better discord)
+    2. vesktop (better discord) (require yay)
     0. Go to the next step
 
     " yn
     case $yn in
-        [1]* ) git clone https://aur.archlinux.org/vesktop-bin.git
-        cd vesktop-bin
-        makepkg -si;;
-        [2]* ) sudo pacman -S discord;;
+        [1]* ) sudo pacman -S --noconfirm discord;;
+        [2]* ) yay -S vesktop-bin;;
         [0]* ) break;;
         * ) echo "invalid option";;
     esac
@@ -150,10 +166,10 @@ clear
 #media
 while true; do
     read -p "Media
-    1. Spotify
+    1. Spotify (require yay)
     2. vlc
     3. jellyfin (server)
-    4. plex (server)
+    4. plex (server) (require yay)
     5. obs studio
     6. krita 
     7. gimp
@@ -163,20 +179,15 @@ while true; do
 
     " yn
     case $yn in
-        [1]* ) git clone https://aur.archlinux.org/spotify.git
-        cd spotify
-        makepkg -si;;
-        [2]* ) sudo pacman -S vlc;;
-        [3]* ) sudo pacman -S jellyfin-server
-        makepkg -si;;
-        [4]* ) git clone https://aur.archlinux.org/plex-media-server.git
-        cd plex-media-server
-        makepkg -si;;
-        [5]* ) sudo pacman -S obs-studio;;
-        [6]* ) sudo pacman -S krita;;
-        [7]* ) sudo pacman -S gimp;;
-        [8]* ) sudo pacman -S inkscape;;
-        [9]* ) sudo pacman -S blender;;
+        [1]* ) yay -S spotify;;
+        [2]* ) sudo pacman -S --noconfirm vlc;;
+        [3]* ) sudo pacman -S --noconfirm jellyfin-server;;
+        [4]* ) yay -S plex-media-server;;
+        [5]* ) sudo pacman -S --noconfirm obs-studio;;
+        [6]* ) sudo pacman -S --noconfirm krita;;
+        [7]* ) sudo pacman -S --noconfirm gimp;;
+        [8]* ) sudo pacman -S --noconfirm inkscape;;
+        [9]* ) sudo pacman -S --noconfirm blender;;
         [0]* ) break;;
         * ) echo "invalid option";;
     esac
@@ -188,23 +199,19 @@ clear
 while true; do
     read -p "documents
     1. libre office fresh
-    2. only office
-    3. open office
+    2. only office (require yay)
+    3. open office (require yay)
     4. obsidian
     5. kate
     0. Go to the next step
 
     " yn
     case $yn in
-        [1]* ) sudo pacman -S libreoffice-fresh;;
-        [2]* ) git clone https://aur.archlinux.org/onlyoffice-bin.git
-        cd onlyoffice-bin
-        makepkg -si;;
-        [3]* ) git clone https://aur.archlinux.org/openoffice-bin.git
-        cd openoffice-bin
-        makepkg -si;;
-        [4]* ) sudo pacman -S obsidian;;
-        [5]* ) sudo pacman -S kate;;
+        [1]* ) sudo pacman -S --noconfirm libreoffice-fresh;;
+        [2]* ) yay -S onlyoffice-bin;;
+        [3]* ) yay -S openoffice-bin;;
+        [4]* ) sudo pacman -S --noconfirm obsidian;;
+        [5]* ) sudo pacman -S --noconfirm kate;;
         [0]* ) break;;
         * ) echo "invalid option";;
     esac
@@ -215,21 +222,44 @@ clear
 #dev tools
 while true; do
     read -p "Dev Tools
-    1. vs codium
+    1. vs codium (require yay)
     2. vs code
     3. node js
     4. docker
-    
+    0. Go to the next step
+
     " yn
     case $yn in
-        [1]* ) git clone https://aur.archlinux.org/vscodium-bin.git
-        cd vscodium-bin
-        makepkg -si;;
-        [2]* ) sudo pacman -S vscode;;
-        [3]* ) sudo pacman -S nodejs;;
-        [4]* ) sudo pacman -S docker;;
+        [1]* ) yay -S vscodium-bin;;
+        [2]* ) sudo pacman -S --noconfirm vscode;;
+        [3]* ) sudo pacman -S --noconfirm nodejs;;
+        [4]* ) sudo pacman -S --noconfirm docker;;
         [0]* ) break;;
         * ) echo "Please answer yes or no.";;
+    esac
+done
+
+clear
+
+#customizion
+while true; do
+    read -p "Customizion
+    1. kde wallpaper engine (unstable) (require plasma 6) (https://aur.archlinux.org/packages/plasma6-wallpapers-wallpaper-engine-git)
+    2. chris titus bash (https://christitus.com/mybash/)
+    3.
+    0. Go to the next step
+
+    " yn
+    case $yn in
+        [1]* ) yay -S kdewallpaperengine;;
+        [2]* ) mkdir -p ~/build
+cd ~/build
+git clone https://github.com/christitustech/mybash
+cd mybash
+./setup.sh;;
+        [3]* ) Echo "thing was installed";;
+        [0]* ) break;;
+        * ) echo "invalid option";;
     esac
 done
 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ArchPis aka (Arch Post Install Setup) is a tool that install basic packages for you (you pick what) and it's download everything needed and installs it
+
+
+TO DO
+set a defualt action with Yes or no question
+make the read me better
+add more packages and feature
+
+add Bottles, protonup-qt, protontricks
+
+
+May do
+have a GUI
